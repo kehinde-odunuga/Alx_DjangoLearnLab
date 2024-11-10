a5254510181329178137427f8a43c1b4e0d1db52
kehinde-odunuga <omobolaji92@gmail.com>
2024-11-10 23:30:44 +0100

    user auth

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -10,9 +10,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='user_login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='user_logout'),
     path('register/', views.register, name='register'),
-    # path('login/', views.user_login, name='login'),
-    # path('logout/', views.user_logout, name='logout'),
-    # path('register/', views.register, name='register'),  
-    # path('/', views.LogoutView.as_view(template_name=))
-    # path('/', views.LoginView.as_view(template_name=)),
 ]
