2366ec3f5c487cd6fa35c1d5a23f4f390a31c72e
kehinde-odunuga <omobolaji92@gmail.com>
2024-11-14 09:01:53 +0100

    role based access

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -77,14 +77,17 @@ def is_member(user):
     return user.userprofile.role == 'Member'
 
 # Views for each role
+@login_required
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
+@login_required
 @user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
 
+@login_required
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
