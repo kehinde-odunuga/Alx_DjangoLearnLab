8f69f095aa1668713fd4ba37b8693dd6ccd82664
kehinde-odunuga <omobolaji92@gmail.com>
2024-11-14 08:53:21 +0100

    role based access

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -68,13 +68,13 @@ def user_login(request):
 
 # Helper functions to check user roles
 def is_admin(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+    return user.userprofile.role == 'Admin'
 
 def is_librarian(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+    return user.userprofile.role == 'Librarian'
 
 def is_member(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+    return user.userprofile.role == 'Member'
 
 # Views for each role
 @user_passes_test(is_admin)
