5230bc08442efb0f9e5e11a3988758a292600ab4
kehinde-odunuga <omobolaji92@gmail.com>
2024-11-24 21:03:10 +0100

    CRUD with viewsets & routers

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
 
 # Create your views here.
 
@@ -9,7 +9,7 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookViewSet(ModelViewSet):
+class BookViewSet(viewsets.ModelViewSet):
     """
     A ViewSet for handling CRUD operations for the Book model.
     """
